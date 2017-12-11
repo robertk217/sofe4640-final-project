8d814149c91f52f3950d7cf228b681608bae5bd6
Robert Kocovski <100536625@oncampus.local>
2017-12-10 20:12:31 -0500

    cleanup

--- app/src/main/java/com/example/a100536625/finalproject/MyAppointments.java
+++ app/src/main/java/com/example/a100536625/finalproject/MyAppointments.java
@@ -89,52 +89,5 @@ public class MyAppointments extends AppCompatActivity
         showEditAppointment(-1);
     }
 
-    /* kept for reference, but this is no longer called */
-    /*private void testModel() {
-        ContactHelper contactHelper = new ContactHelper(this);
-
-        contactHelper.deleteAllContacts();
-
-        Contact bsmith = contactHelper.createContact("Barb",
-                "Smith",
-                "bsmith@uoit.ca",
-                "905-721-8668");
-
-        Contact lramirez = contactHelper.createContact("Luis",
-                "Ramirez",
-                "lramirez@utoronto.ca",
-                "416-123-4567");
-
-        Contact deleteMe = contactHelper.createContact("Tobe",
-                "Deleted",
-                "deleteme@soon.com",
-                "213-888-9999");
-
-        Log.i("SQLite", "getAllContacts():");
-        List<Contact> allContacts = contactHelper.getAllContacts();
-        for (int i = 0; i < allContacts.size(); i++) {
-            Contact current = allContacts.get(i);
-            Log.i("SQLite", current.getFirstName() + " " + current.getLastName() +
-                    " - " + current.getPhone());
-        }
-
-        Contact lramirez2 = contactHelper.getContact(lramirez.getId());
-        Log.i("SQLite", "getContact():");
-        Log.i("SQLite", lramirez2.getFirstName() + " " + lramirez2.getLastName());
-
-        lramirez2.setPhone("111-222-3333");
-        boolean updateSuccess = contactHelper.updateContact(lramirez2);
-        Log.i("SQLite", "updateSuccess == " + updateSuccess);
-
-        boolean deleteSuccess = contactHelper.deleteContact(deleteMe.getId());
-        Log.i("SQLite", "deleteSuccess == " + deleteSuccess);
-
-        Log.i("SQLite", "getAllContacts():");
-        allContacts = contactHelper.getAllContacts();
-        for (int i = 0; i < allContacts.size(); i++) {
-            Contact current = allContacts.get(i);
-            Log.i("SQLite", current.getFirstName() + " " + current.getLastName() +
-                    " - " + current.getPhone());
-        }
-    } */
+    
 }
